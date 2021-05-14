22e62e4de139bd5c14999dfafcdcf1eba7d8fd08
soer3n <srenhenning@googlemail.com>
2021-05-14 19:13:47 +0200

    remove moved func

--- pkg/helm/main.go
+++ pkg/helm/main.go
@@ -6,11 +6,8 @@ import (
 	"path/filepath"
 
 	"github.com/pkg/errors"
-	helmv1alpha1 "github.com/soer3n/apps-operator/apis/helm/v1alpha1"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/cli"
-	"helm.sh/helm/v3/pkg/repo"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func initActionConfig(settings *cli.EnvSettings) (*action.Configuration, error) {
@@ -93,47 +90,4 @@ func (c HelmClient) GetEnvSettings() *cli.EnvSettings {
 	}
 
 	return settings
-}
-
-func AddOrUpdateChartMap(chartObjMap map[string]*helmv1alpha1.Chart, chartMeta *repo.ChartVersion, instance *helmv1alpha1.Repo) (map[string]*helmv1alpha1.Chart, error) {
-
-	_, ok := chartObjMap[chartMeta.Name]
-
-	if ok {
-		chartObjMap[chartMeta.Name].Spec.Versions = append(chartObjMap[chartMeta.Name].Spec.Versions, chartMeta.Version)
-		return chartObjMap, nil
-	}
-
-	helmChart := &helmv1alpha1.Chart{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      chartMeta.Name,
-			Namespace: instance.ObjectMeta.Namespace,
-			Labels: map[string]string{
-				"chart":     chartMeta.Name,
-				"repo":      instance.Spec.Name,
-				"repoGroup": instance.ObjectMeta.Labels["repoGroup"],
-			},
-		},
-		Spec: helmv1alpha1.ChartSpec{
-			Name:        chartMeta.Name,
-			Home:        chartMeta.Home,
-			Sources:     chartMeta.Sources,
-			Versions:    []string{chartMeta.Version},
-			Description: chartMeta.Description,
-			Keywords:    chartMeta.Keywords,
-			Maintainers: chartMeta.Maintainers,
-			Icon:        chartMeta.Icon,
-			APIVersion:  chartMeta.APIVersion,
-			Condition:   chartMeta.Condition,
-			Tags:        chartMeta.Tags,
-			AppVersion:  chartMeta.AppVersion,
-			Deprecated:  chartMeta.Deprecated,
-			Annotations: chartMeta.Annotations,
-			KubeVersion: chartMeta.KubeVersion,
-			Type:        chartMeta.Type,
-		},
-	}
-
-	chartObjMap[chartMeta.Name] = helmChart
-	return chartObjMap, nil
 }
