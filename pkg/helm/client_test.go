1fc5df0a0c19f3133a775bd535968e55564f943b
soer3n <srenhenning@googlemail.com>
2021-06-28 20:11:33 +0200

    add helmclient test skeleton

---
+++ pkg/helm/client_test.go
@@ -0,0 +1,73 @@
+package helm
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	helmv1alpha1 "github.com/soer3n/apps-operator/apis/helm/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func TestClient(t *testing.T) {
+
+	clientMock := K8SClientMock{}
+	httpMock := HTTPClientMock{}
+
+	clientMock.On("List", context.Background(), &helmv1alpha1.ChartList{}, []client.ListOption{client.InNamespace(""), client.MatchingLabels{
+		"label": "selector",
+	}}).Return(nil).Run(func(args mock.Arguments) {
+
+		_ = args.Get(1).(*helmv1alpha1.ChartList)
+	})
+
+	clientMock.On("List", context.Background(), &helmv1alpha1.ChartList{}, []client.ListOption{client.InNamespace(""), client.MatchingLabels{}}).Return(nil).Run(func(args mock.Arguments) {
+
+		_ = args.Get(1).(*helmv1alpha1.ChartList)
+	})
+
+	clientMock.On("List", context.Background(), &helmv1alpha1.ChartList{}, []client.ListOption{client.InNamespace(""), client.MatchingLabels{
+		"label": "notpresent",
+	}}).Return(nil).Run(func(args mock.Arguments) {
+
+		_ = args.Get(1).(*helmv1alpha1.ChartList)
+	})
+
+	/*expected :=  getExpectedTestCharts(clientMock)*/
+
+	indexFile := getTestIndexFile()
+	rawIndexFile, _ := json.Marshal(indexFile)
+	httpResponse := &http.Response{
+		Body: ioutil.NopCloser(bytes.NewReader(rawIndexFile)),
+	}
+
+	httpMock.On("Get",
+		"https://foo.bar/charts/index.yaml").Return(httpResponse, nil)
+
+	assert := assert.New(t)
+
+	testObj := NewHelmClient(getTestClientRepo(), &clientMock, &httpMock)
+	repoObj := testObj.GetRepo("")
+
+	// assert.Equal(expected, charts, "Structs should be equal.")
+	assert.Nil(repoObj)
+
+	testObj = NewHelmClient(getTestClientRelease(), &clientMock, &httpMock)
+	releaseObj := testObj.GetRelease("", "")
+
+	// assert.Equal(expected, charts, "Structs should be equal.")
+	assert.Nil(releaseObj)
+}
+
+func getTestClientRepo() *HelmRepo {
+	return &HelmRepo{}
+}
+
+func getTestClientRelease() *HelmRelease {
+	return &HelmRelease{}
+}
