247f592eb80f7dc0f78d3cae44915cea3f2523b5
soer3n <srenhenning@googlemail.com>
2021-06-29 12:45:48 +0200

    add more cov

--- apis/helm/v1alpha1/chart_test.go
+++ apis/helm/v1alpha1/chart_test.go
@@ -4,16 +4,89 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"helm.sh/helm/v3/pkg/chart"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestGetChartVersion(t *testing.T) {
 
+	testChartSpec := &Chart{
+		Spec: ChartSpec{
+			Versions: []ChartVersion{
+				{
+					Name: "0.0.1",
+				},
+				{
+					Name: "0.0.3",
+				},
+			},
+		},
+	}
+
 	assert := assert.New(t)
-	assert.NotNil("gfo")
+
+	cv := testChartSpec.GetChartVersion("0.0.1")
+	assert.NotNil(cv)
+
+	cv = testChartSpec.GetChartVersion("0.0.2")
+	assert.Equal(&ChartVersion{}, cv)
+
+	cv = testChartSpec.GetChartVersion("0.0.3")
+	assert.NotNil(cv)
+
+	cv = testChartSpec.GetChartVersion("fasfhoiqef")
+	assert.Equal(&ChartVersion{}, cv)
+
+	testChartSpec.Spec.Versions = make([]ChartVersion, 0)
+	testChartSpec.Spec.Versions = append(testChartSpec.Spec.Versions, ChartVersion{
+		Name: "fdsifhdsogb",
+	})
+
+	cv = testChartSpec.GetChartVersion("0.0.1")
+	assert.Equal(&ChartVersion{}, cv)
 }
 
 func TestConvertChartVersions(t *testing.T) {
 
+	testChartSpec := &Chart{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "foo",
+		},
+		Spec: ChartSpec{
+			Name:        "foo",
+			Home:        "home",
+			Sources:     []string{"source"},
+			Description: "desc",
+			Keywords:    []string{"key", "word"},
+			Maintainers: []*chart.Maintainer{},
+			Icon:        "icon",
+			APIVersion:  "apiversion",
+			Condition:   "cond",
+			Tags:        "tags",
+			AppVersion:  "appversion",
+			Deprecated:  false,
+			Annotations: map[string]string{},
+			KubeVersion: "new",
+			Type:        "type",
+			Versions: []ChartVersion{
+				{
+					Name: "0.0.1",
+				},
+				{
+					Name: "0.0.3",
+					Dependencies: []ChartDep{
+						{
+							Name:    "dep",
+							Repo:    "dep",
+							Version: "0.0.1",
+						},
+					},
+				},
+			},
+		},
+	}
 	assert := assert.New(t)
-	assert.NotNil("gfo")
+
+	cvs := testChartSpec.ConvertChartVersions()
+	assert.Len(cvs, 2)
 }
--- pkg/utils/main_test.go
+++ pkg/utils/main_test.go
@@ -4,16 +4,45 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestContains(t *testing.T) {
 
+	testList := []string{"foo", "bar"}
+
 	assert := assert.New(t)
-	assert.NotNil("gfo")
+
+	ok := Contains(testList, "foo")
+	assert.True(ok)
+
+	ok = Contains(testList, "fuz")
+	assert.False(ok)
 }
 
 func TestGetLabelsByInstance(t *testing.T) {
 
+	metaObj := metav1.ObjectMeta{
+		Name:      "foo",
+		Namespace: "bar",
+		Labels: map[string]string{
+			"repo": "repo",
+		},
+	}
+
+	envMap := map[string]string{
+		"RepositoryConfig": "/tmp/.config/helm",
+		"RepositoryCache":  "/tmp/.cache/helm",
+	}
+
 	assert := assert.New(t)
-	assert.NotNil("gfo")
+
+	config, cache := GetLabelsByInstance(metaObj, envMap)
+	assert.NotNil(config)
+	assert.NotNil(cache)
+
+	metaObj.Labels["repoGroup"] = "group"
+	config, cache = GetLabelsByInstance(metaObj, envMap)
+	assert.NotNil(config)
+	assert.NotNil(cache)
 }
