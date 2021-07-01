5fa5d82fbd5b061f397cfb97fd13fbf616793883
soer3n <srenhenning@googlemail.com>
2021-07-01 12:11:36 +0200

    add init http req params mocking

--- pkg/api/handlers_test.go
+++ pkg/api/handlers_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/soer3n/apps-operator/internal/mocks"
 	"github.com/soer3n/apps-operator/pkg/client"
 	"github.com/stretchr/testify/assert"
@@ -37,15 +38,74 @@ func TestK8sApiGroup(t *testing.T) {
 	k8sclient.DynamicClient = dcMock
 
 	res := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodGet, "/", res.Body)
+	req := httptest.NewRequest(http.MethodGet, "/api/resources/", res.Body)
 
 	handler := NewHandler("v1", k8sclient)
 	handler.K8sApiGroupResources(res, req)
-	assert.NotNil("gfo")
+	assert.NotNil(res)
+
+	res = httptest.NewRecorder()
+	req = httptest.NewRequest(http.MethodGet, "/api/resources/resource", res.Body)
+	req = mux.SetURLVars(req, map[string]string{
+		"resource": "resource",
+	})
+
+	handler = NewHandler("v1", k8sclient)
+	handler.K8sApiGroupResources(res, req)
+	assert.NotNil(res)
+
+	res = httptest.NewRecorder()
+	req = httptest.NewRequest(http.MethodGet, "/api/resources/resource/group", res.Body)
+	req = mux.SetURLVars(req, map[string]string{
+		"resource": "resource",
+		"group":    "group",
+	})
+
+	handler = NewHandler("v1", k8sclient)
+	handler.K8sApiGroupResources(res, req)
+	assert.NotNil(res)
+
+	req = httptest.NewRequest(http.MethodGet, "/api/resources/resource/group/version", nil)
+	res = httptest.NewRecorder()
+	req = mux.SetURLVars(req, map[string]string{
+		"resource": "resource",
+		"group":    "group",
+		"version":  "v1",
+	})
+
+	handler = NewHandler("v1", k8sclient)
+	handler.K8sApiGroupResources(res, req)
+	assert.NotNil(res)
 }
 
 func TestK8sApiGroupResources(t *testing.T) {
 
 	assert := assert.New(t)
-	assert.NotNil("gfo")
+
+	dcMock := &mocks.K8SDynamicClientMock{}
+
+	gvr := schema.GroupVersionResource{
+		Group:    "group",
+		Version:  "v1",
+		Resource: "resource",
+	}
+	namespaceableMock := mocks.K8SNamespaceMock{}
+	dcMock.On("Resource", gvr).Return(&namespaceableMock)
+
+	resourceMock := mocks.K8SResourceMock{}
+	namespaceableMock.On("Namespace", "namespace").Return(&resourceMock)
+
+	usObj := &unstructured.UnstructuredList{}
+	resourceMock.On("List", metav1.ListOptions{}).Return(usObj, nil)
+
+	k8sclient := client.New()
+	k8sclient.DynamicClient = dcMock
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	res := httptest.NewRecorder()
+
+	handler := NewHandler("v1", k8sclient)
+	handler.K8sApiGroup(res, req)
+	assert.NotNil(res)
+
 }
