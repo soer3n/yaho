c0655c426f798e3ddea5c65575b9545d9c8a6242
soer3n <srenhenning@googlemail.com>
2021-07-01 10:47:52 +0200

    add http handler mocking

--- internal/mocks/types.go
+++ internal/mocks/types.go
@@ -1,6 +1,8 @@
 package mocks
 
 import (
+	"net/http"
+
 	"github.com/soer3n/apps-operator/internal/types"
 	"github.com/stretchr/testify/mock"
 	"k8s.io/client-go/discovery"
@@ -16,6 +18,11 @@ type K8SClientMock struct {
 type HTTPClientMock struct {
 	mock.Mock
 	types.HTTPClientInterface
+}
+
+type HTTPResponseMock struct {
+	mock.Mock
+	http.ResponseWriter
 }
 
 type K8SResourceMock struct {
--- pkg/api/handlers_test.go
+++ pkg/api/handlers_test.go
@@ -1,15 +1,46 @@
 package api
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/soer3n/apps-operator/internal/mocks"
+	"github.com/soer3n/apps-operator/pkg/client"
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 func TestK8sApiGroup(t *testing.T) {
 
 	assert := assert.New(t)
 
+	dcMock := &mocks.K8SDynamicClientMock{}
+
+	gvr := schema.GroupVersionResource{
+		Group:    "group",
+		Version:  "v1",
+		Resource: "resource",
+	}
+	namespaceableMock := mocks.K8SNamespaceMock{}
+	dcMock.On("Resource", gvr).Return(&namespaceableMock)
+
+	resourceMock := mocks.K8SResourceMock{}
+	namespaceableMock.On("Namespace", "namespace").Return(&resourceMock)
+
+	usObj := &unstructured.UnstructuredList{}
+	resourceMock.On("List", metav1.ListOptions{}).Return(usObj, nil)
+
+	k8sclient := client.New()
+	k8sclient.DynamicClient = dcMock
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/", res.Body)
+
+	handler := NewHandler("v1", k8sclient)
+	handler.K8sApiGroupResources(res, req)
 	assert.NotNil("gfo")
 }
 
